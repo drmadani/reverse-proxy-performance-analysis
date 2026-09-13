62d9d90f37731733bf2e6a81babc885fd0a11ebd
drmadani <110072643+drmadani@users.noreply.github.com>
2026-09-13 15:12:59 +0700

    Create skenario1.js

---
+++ scripts/skenario1.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '1m', target: 100 },
+    { duration: '1m', target: 300 },
+    { duration: '1m', target: 500 },
+    { duration: '1m', target: 700 },
+    { duration: '1m', target: 1000 },
+    { duration: '1m', target: 0 },
+  ],
+};
+
+export default function () {
+  const res = http.get(__ENV.TARGET_URL);
+  check(res, { 'status 200': (r) => r.status === 200 });
+  sleep(1);
+}
